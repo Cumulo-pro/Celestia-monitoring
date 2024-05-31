e322fe8ccf86e42023d543856088fcefeaca09da
Mon | Cumulo <moncho.santosdios@gmail.com>
2024-05-31 22:12:19 +0200

    Update get_bridge_height.sh

--- bridge-monitor/get_bridge_height.sh
+++ bridge-monitor/get_bridge_height.sh
@@ -29,40 +29,82 @@ type_comment="# TYPE bridge_height gauge"
 
 
 # The last version running on Celestia
-last_vers_danode=$(sudo journalctl -u celestia-bridge.service | awk '/node version:/ {last_version=$NF} END {gsub(/\./, "", last_version); print last_version/10}')
-echo "# HELP last_vers_danode The last version running on Celestia Bridge" >> "$metrics_file"
-echo "# TYPE last_vers_danode gauge" >> "$metrics_file"
-echo "last_vers_danode $last_vers_danode" >> "$metrics_file"
+last_vers_danode=$(sudo journalctl -u celestia-bridge.service | grep "node version:" | tail -n 1 | awk '{print substr($NF, 2)}' | tr -d '.' | awk '{print $1 / 10}')
+# Define HELP y TYPE
+help_comment="# HELP last_vers_danode The last version running on Celestia Bridge"
+type_comment="# TYPE last_vers_danode gauge"
+# Save last_vers_danode
+{
+    echo "$help_comment"
+    echo "$type_comment"
+    echo "last_vers_danode $last_vers_danode"
+} >> "$metrics_file"
 
 # Counting the number of network connectivity timeout errors
-num_err_timeout_connectivity=$(sudo journalctl -p err | grep -c 'Timeout occurred while waiting for network connectivity')
-echo "# HELP num_err_timeout_connectivity Counting the number of network connectivity timeout errors" >> "$metrics_file"
-echo "# TYPE num_err_timeout_connectivity gauge" >> "$metrics_file"
-echo "num_err_timeout_connectivity $num_err_timeout_connectivity" >> "$metrics_file"
+num_err_timeout_connectivity=$(sudo journalctl -p err | grep 'Timeout occurred while waiting for network connectivity' | wc -l)
+# Define HELP and TYPE
+help_comment="# HELP num_err_timeout_connectivity Counting the number of network connectivity timeout errors"
+type_comment="# TYPE num_err_timeout_connectivity gauge"
+# Save
+{
+    echo "$help_comment"
+    echo "$type_comment"
+    echo "num_err_timeout_connectivity $num_err_timeout_connectivity"
+} >> "$metrics_file"
 
 # Counting the number of closed connections per remote host
 num_closed_remote_host=$(sudo journalctl -p err | grep -c 'Connection closed by remote host')
-echo "# HELP num_closed_remote_host Counting the number of closed connections per remote host" >> "$metrics_file"
-echo "# TYPE num_closed_remote_host gauge" >> "$metrics_file"
-echo "num_closed_remote_host $num_closed_remote_host" >> "$metrics_file"
+# Define HELP and TYPE for num_closed_remote_host
+help_comment="# HELP num_closed_remote_host Counting the number of closed connections per remote host"
+type_comment="# TYPE num_closed_remote_host gauge"
+# Save num_closed_remote_host metrics to file
+{
+    echo "$help_comment"
+    echo "$type_comment"
+    echo "num_closed_remote_host $num_closed_remote_host"
+} >> "$metrics_file"
 
 # Counting the number of time stamp analysis errors
-num_failed_parse_timestamp=$(sudo journalctl -p err | grep -c "Failed to parse timestamp: '1'")
-echo "# HELP num_failed_parse_timestamp Counting the number of time stamp analysis errors" >> "$metrics_file"
-echo "# TYPE num_failed_parse_timestamp gauge" >> "$metrics_file"
-echo "num_failed_parse_timestamp $num_failed_parse_timestamp" >> "$metrics_file"
+num_failed_parse_timestamp=$(sudo journalctl -p err | grep -c "Failed to parse timestamp: '1")
+# Define HELP and TYPE for num_failed_parse_timestamp
+help_comment="# HELP num_failed_parse_timestamp Counting the number of time stamp analysis errors"
+type_comment="# TYPE num_failed_parse_timestamp gauge"
+# Save num_failed_parse_timestamp metrics to file
+{
+    echo "$help_comment"
+    echo "$type_comment"
+    echo "num_failed_parse_timestamp $num_failed_parse_timestamp"
+} >> "$metrics_file"
 
 # Obtain connection status
-connection_status=$(sudo journalctl -u celestia-bridge.service | awk '/CANONICAL_PEER_STATUS:/ {status=$NF} END {if (status=="established") print 1; else print 0}')
-echo "# HELP connection_status Connection status of the bridge node (1 for established, 0 otherwise)" >> "$metrics_file"
-echo "# TYPE connection_status gauge" >> "$metrics_file"
-echo "connection_status $connection_status" >> "$metrics_file"
+status=$(sudo journalctl -u celestia-bridge.service | grep "CANONICAL_PEER_STATUS:" | awk -F'connection_status="' '{print $2}' | cut -d'"' -f1 | tail -n 1)
+# Define HELP and TYPE for connection status
+help_comment_status="# HELP connection_status Connection status of the bridge node (1 for established, 0 otherwise)"
+type_comment_status="# TYPE connection_status gauge"
+# Determine connection status value (1 if established, 0 otherwise)
+if [ "$status" = "established" ]; then
+    connection_status_value=1
+else
+    connection_status_value=0
+fi
+# Save
+{
+    echo "$help_comment_status"
+    echo "$type_comment_status"
+    echo "connection_status $connection_status_value"
+} > "$metrics_file"
 
 # Obtain chain_id_danode status
-chain_id_danode=$(sudo journalctl -u celestia-bridge.service | awk '/network:/ {if ($NF=="mocha-4") print 0; else if ($NF=="celestia") print 1}' | tail -n 1)
-echo "# HELP chain_id_danode Network status of the bridge node (1 for celestia, 0 for mocha-4)" >> "$metrics_file"
-echo "# TYPE chain_id_danode gauge" >> "$metrics_file"
-echo "chain_id_danode $chain_id_danode" >> "$metrics_file"
+chain_id_danode=$(sudo journalctl -u celestia-bridge.service | grep "network:" | awk '{if ($NF == "mocha-4") print 0; else if ($NF == "celestia") print 1}' | tail -n 1)
+# Define HELP and TYPE for chain_id_danode
+help_comment_chain_id="# HELP chain_id_danode Network status of the bridge node (1 for celestia, 0 for mocha-4)"
+type_comment_chain_id="# TYPE chain_id_danode gauge"
+# Save metrics to the file
+{
+    echo "$help_comment_chain_id"
+    echo "$type_comment_chain_id"
+    echo "chain_id_danode $chain_id_danode"
+} > "$metrics_file"
 
 # Time since last connection closed by remote host
 last_closed_remote_host=$(sudo journalctl -p err | awk '/Connection closed by remote host/ {date=$1 " " $2 " " $3} END {print date}')
