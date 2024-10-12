fb2d168eb300cc2d245fdeac456897e73c0e3caf
Mon | Cumulo <moncho.santosdios@gmail.com>
2024-10-12 18:15:37 +0200

    Update info_bridge.sh

--- bridge-monitor/info_bridge.sh
+++ bridge-monitor/info_bridge.sh
@@ -26,11 +26,8 @@ sudo bash -c "{
 # Obtener la ruta del directorio de almacenamiento del nodo Celestia
 node_store_path="$HOME/.celestia-bridge-mocha-4"
 
-# Obtener la ruta del directorio de almacenamiento del nodo Celestia
-node_store_path="$HOME/.celestia-bridge-mocha-4"
-
-# Obtener la ID del nodo (Node ID) y depurar el resultado, preservando el valor de $HOME del usuario actual
-node_id=$(sudo -E /usr/local/bin/celestia p2p info --node.store $node_store_path 2>&1)
+# Obtener la ID del nodo (Node ID) usando la ruta de almacenamiento con sudo
+node_id=$(sudo /usr/local/bin/celestia p2p info --node.store "$node_store_path" 2>&1)
 
 # Depuración del resultado bruto
 echo "DEBUG: Raw output from celestia command: $node_id"
@@ -54,6 +51,7 @@ sudo bash -c "{
     echo '# TYPE node_id_info gauge'
     echo 'node_id_info{id=\"$node_id\"} 1'
 } >> $temp_metrics_file"
+
 
 # Obtener el hash de la altura actual (Hash Current Height)
 hash_current_height=$(sudo journalctl -u celestia-bridge.service | grep 'new head' | tail -n 1 | awk -F 'hash": "' '{print $2}' | awk -F '"' '{print $1}')
